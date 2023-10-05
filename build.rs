cdff99bb696c3d7bcf63ee814e4eef44cb4c92d4
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-10-05 15:51:50 +0200

    added omp flags handling depending on compiler

--- build.rs
+++ build.rs
@@ -2,20 +2,30 @@ use std::env;
 
 fn main() {
     // need to find a good default value for the compiler
-    let compiler = env::var("CXX").unwrap_or("clang++".to_string());
-    println!("{}", compiler);
+    let compiler = env::var("CXX").unwrap_or("g++".to_string());
+
+    // messy but works
+    let ompflags = if compiler.contains("g++") {
+        // g++ flavor
+        "-fopenmp"
+    } else if compiler.contains("clang++") {
+        // clang++ flavor
+        "-fopenmp=libomp"
+    } else {
+        panic!()
+    };
+
     cxx_build::bridge("src/lib.rs")
         .compiler(compiler)
         .file("src/hello.cpp")
         .flag_if_supported("-std=c++20")
-        .flag_if_supported("-fopenmp") // gcc not working ? missing this flag when compiling with g++
-        .flag_if_supported("-fopenmp=libomp") // clang
+        .flag(ompflags) // clang
         .compile("poc-cc");
 
     // --- linking shenanigans ---
     println!("cargo:rustc-link-arg=-lomp");
     if env::consts::OS == "macos" {
-        println!("cargo:rustc-link-arg=-L/opt/homebrew/Cellar/llvm/17.0.2/lib");
+        println!("cargo:rustc-link-arg=-L/opt/homebrew/opt/libomp/lib");
         println!("cargo:rustc-link-arg=-ld_classic");
     }
     println!("cargo:rerun-if-changed=src/main.rs");
