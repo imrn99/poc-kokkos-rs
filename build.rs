adc72e647648ec049b3f024af644426b65e89c10
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-10-05 15:37:20 +0200

    macos specific flags ok
    need to fix compiling flags & linux ldflags

--- build.rs
+++ build.rs
@@ -8,13 +8,16 @@ fn main() {
         .compiler(compiler)
         .file("src/hello.cpp")
         .flag_if_supported("-std=c++20")
-        .flag("-fopenmp") // gcc not working ? missing this flag when compiling with g++
+        .flag_if_supported("-fopenmp") // gcc not working ? missing this flag when compiling with g++
         .flag_if_supported("-fopenmp=libomp") // clang
         .compile("poc-cc");
 
-    println!("cargo:rustc-link-arg=-L/opt/homebrew/Cellar/llvm/17.0.2/lib");
-    //println!("cargo:rustc-link-arg=-lomp");
-    //println!("cargo:rustc-link-arg=-ld_classic"); // xcode 15.0 broke the linker :))))))))
+    // --- linking shenanigans ---
+    println!("cargo:rustc-link-arg=-lomp");
+    if env::consts::OS == "macos" {
+        println!("cargo:rustc-link-arg=-L/opt/homebrew/Cellar/llvm/17.0.2/lib");
+        println!("cargo:rustc-link-arg=-ld_classic");
+    }
     println!("cargo:rerun-if-changed=src/main.rs");
     println!("cargo:rerun-if-changed=src/hello.cpp");
     println!("cargo:rerun-if-changed=include/hello.hpp");
