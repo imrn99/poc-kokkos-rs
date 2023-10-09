604e3b6e9a6121ca671ea6937f16903bc86ae89d
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-10-09 09:29:14 +0200

    added macros for basic dim/stride implem

--- src/traits/dimension.rs
+++ src/traits/dimension.rs
@@ -1,46 +1,61 @@
 use std::ops::Index;
 
+// Dim
+
 pub trait Dim: Index<usize> {
     fn depth(&self) -> usize;
     fn val(&self) -> &[usize];
 }
 
-//TODO: write a macro that generates these blocks
+macro_rules! impl_dim {
+    ($n:expr) => {
+        impl Dim for [usize; $n] {
+            fn depth(&self) -> usize {
+                self.len()
+            }
 
-impl Dim for [usize; 1] {
-    fn depth(&self) -> usize {
-        self.len()
-    }
-
-    fn val(&self) -> &[usize] {
-        self
-    }
+            fn val(&self) -> &[usize] {
+                self
+            }
+        }
+    };
 }
+
+impl_dim!(1);
+impl_dim!(2);
+impl_dim!(3);
+impl_dim!(4);
+impl_dim!(5);
+impl_dim!(6);
+impl_dim!(7);
+impl_dim!(8);
+
+// Stride
 
 pub trait Stride: Copy + Index<usize> {
     fn depth(&self) -> usize;
     fn val(&self) -> &[usize];
 }
 
-//TODO: write a macro that generates these blocks
+macro_rules! impl_stride {
+    ($n:expr) => {
+        impl Stride for [usize; $n] {
+            fn depth(&self) -> usize {
+                self.len()
+            }
 
-// huh?
-impl Stride for [usize; 0] {
-    fn depth(&self) -> usize {
-        self.len()
-    }
-    // huh?
-    fn val(&self) -> &[usize] {
-        self
-    }
+            fn val(&self) -> &[usize] {
+                self
+            }
+        }
+    };
 }
 
-impl Stride for [usize; 1] {
-    fn depth(&self) -> usize {
-        self.len()
-    }
-
-    fn val(&self) -> &[usize] {
-        self
-    }
-}
+impl_stride!(1);
+impl_stride!(2);
+impl_stride!(3);
+impl_stride!(4);
+impl_stride!(5);
+impl_stride!(6);
+impl_stride!(7);
+impl_stride!(8);
