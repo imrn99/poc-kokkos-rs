364868fb57d17a4b1ba291905de64faaf470419c
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-10-09 10:19:13 +0200

    replaced dim/stride traits with SmallVec type
    generic traits made stride computing too much of a hassle

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -3,22 +3,24 @@
 //!
 //!
 
+use crate::traits::data::SomeData;
+use smallvec::SmallVec;
 use std::sync::Arc;
-
-use crate::traits::{
-    data::SomeData,
-    dimension::{Dim, Stride},
-};
 
 /// Maximum possible depth (i.e. number of dimensions) for a view.
 pub const MAX_VIEW_DEPTH: usize = 8;
 
+/// Dim
+
+pub type Dimension = SmallVec<[usize; MAX_VIEW_DEPTH]>;
+
+/// Stride
+pub type Stride = SmallVec<[usize; MAX_VIEW_DEPTH]>;
+
+#[derive(Clone)]
 /// Enum used to represent data layout. Struct enums is used in order to increase
 /// readability.
-pub enum Layout<S>
-where
-    S: Stride,
-{
+pub enum Layout {
     /// Highest stride for the first index, decreasing stride as index increases.
     /// Exact stride for each index can be computed from dimensions at view initialization.
     Right,
@@ -26,53 +28,49 @@ where
     /// Exact stride for each index can be computed from dimensions at view initialization.
     Left,
     /// Custom stride for each index. Must be compatible with dimensions.
-    Stride { s: S },
+    Stride { s: Stride },
 }
 
 /// Common structure used as the backend of all `View` types. The main differences between
 /// usable types is the type of the `data` field.
-pub struct ViewBase<T, S, D>
+pub struct ViewBase<T>
 where
     T: SomeData,
-    S: Stride,
-    D: Dim,
 {
     /// Data container. Depending on the type, it can be a vector (`Owned`), a reference
     /// (`ReadOnly`), a mutable reference (`ReadWrite`) or an `Arc` pointing on a vector
     /// (`Shared`).
     pub data: T,
     /// Memory layout of the view. Refer to Kokkos documentation for more information.
-    pub layout: Layout<S>,
+    pub layout: Layout,
     /// Dimensions of the data represented by the view. The view can:
     /// - be a vector (1 dimension)
     /// - be a multi-dimensionnal array (up to 8 dimensions)
     /// The number of dimensions is referred to as the _depth_. Dimension 0, i.e. scalar,
     /// is not directly supported.
-    pub dim: D,
+    pub dim: Dimension,
     /// Stride between each element of a given dimension. Computed automatically for
     /// [Layout::Left] and [Layout::Right].
-    pub stride: S,
+    pub stride: Stride,
 }
 
-impl<T, S, D> ViewBase<T, S, D>
+impl<T> ViewBase<T>
 where
     T: SomeData,
-    S: Stride,
-    D: Dim,
 {
     /// Constructor used to create owned (and shared?) views. See dedicated methods for
     /// others.
-    pub fn new(data: T, layout: Layout<S>, dim: D) -> Self {
-        let depth = dim.depth();
+    pub fn new(data: T, layout: Layout, dim: Dimension) -> Self {
+        let depth = dim.len();
         // compute stride if necessary
-        let stride = match layout {
+        let stride = match layout.clone() {
             Layout::Right => todo!(),
             Layout::Left => todo!(),
             Layout::Stride { s } => s,
         };
 
         // checks
-        assert_eq!(depth, stride.depth());
+        assert_eq!(depth, stride.len());
 
         // build & return
         Self {
@@ -85,16 +83,16 @@ where
 }
 
 /// View type owning the data it yields access to, i.e. "original" view.
-pub type ViewOwned<A, S, D> = ViewBase<Vec<A>, S, D>;
+pub type ViewOwned<A> = ViewBase<Vec<A>>;
 
 /// View type owning a read-only borrow to the data it yields access to, i.e. a
 /// read-only mirror.
-pub type ViewRO<'a, A, S, D> = ViewBase<&'a [A], S, D>;
+pub type ViewRO<'a, A> = ViewBase<&'a [A]>;
 
 /// View type owning a mutable borrow to the data it yields access to, i.e. a
 /// read-write mirror.
-pub type ViewRW<'a, A, S, D> = ViewBase<&'a mut [A], S, D>;
+pub type ViewRW<'a, A> = ViewBase<&'a mut [A]>;
 
 /// View type owning a shared reference to the data it yields access to, i.e. a
 /// thread-safe read-only mirror.
-pub type ViewShared<A, S, D> = ViewBase<Arc<Vec<A>>, S, D>;
+pub type ViewShared<A> = ViewBase<Arc<Vec<A>>>;
