d14abdbfd49ce1beacf9873fa1ec1d77ceeb0a53
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-10-12 15:20:32 +0200

    rewrote access bench to use random accesses
    weird behavior on 3D unchecked accesses; cf. comments in code

--- benches/view_access.rs
+++ benches/view_access.rs
@@ -1,100 +1,98 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use poc_kokkos_rs::view::{parameters::Layout, ViewOwned};
+use rand::prelude::*;
 
 // this bench is used to evaluate the cost of accessing views' data
+// all benched functions contain 10^6 accesses.
 
 // 1D vector access
-fn f1(size: u32) {
-    let length = 2_usize.pow(size);
-    let mut y: Vec<f64> = vec![0.0; length];
-    black_box(&mut y); // necessary?
+fn f1(length: usize, indices: &[usize]) {
+    let y: Vec<f64> = vec![0.0; length];
+    let idx = &indices[0..length];
 
     for i in 0..1_000_000 {
-        // smth like that to prevent bias from cache usage?
-        black_box(y[(i << 0x01) % length]);
+        let tmp = y[idx[i % length]];
+        black_box(tmp);
     }
 }
 
 // 1D view access
-fn f1_b(size: u32) {
-    let length = 2_usize.pow(size);
-    let mut v_y: ViewOwned<'_, 1, f64> =
+fn f1_b(length: usize, indices: &[usize]) {
+    let v_y: ViewOwned<'_, 1, f64> =
         ViewOwned::new_from_data(vec![0.0; length], Layout::Right, [length]);
-    black_box(&mut v_y); // necessary?
+    let idx = &indices[0..length];
 
     for i in 0..1_000_000 {
-        black_box(v_y[[(i << 0x01) % length]]);
+        let tmp = v_y[[idx[i % length]]];
+        black_box(tmp);
     }
 }
 
 // 2D vector access
-fn f2(size: u32) {
-    let length = 2_usize.pow(size);
-    let mut y: Vec<f64> = vec![0.0; length * length];
-    black_box(&mut y); // necessary?
+fn f2(length: usize, indices: &[usize]) {
+    let y: Vec<f64> = vec![0.0; length * length];
+    let idx = &indices[0..length];
 
-    // simulate 2 indexes ?
     for i in 0..1_000 {
-        for j in 0..1_000 {
-            black_box(y[((i << 0x01) % length) * length + ((j >> 0x01) % length)]);
+        for j in 1_000..2_000 {
+            let tmp = unsafe { y.get_unchecked(idx[i % length] * length + idx[j % length]) };
+            black_box(tmp);
         }
     }
 }
 
 // 2D view access
-fn f2_b(size: u32) {
-    let length = 2_usize.pow(size);
-    let mut v_y: ViewOwned<'_, 2, f64> =
+fn f2_b(length: usize, indices: &[usize]) {
+    let v_y: ViewOwned<'_, 2, f64> =
         ViewOwned::new_from_data(vec![0.0; length * length], Layout::Right, [length, length]);
-    black_box(&mut v_y); // necessary?
+    let idx = &indices[0..length];
 
     for i in 0..1_000 {
-        for j in 0..1_000 {
-            black_box(v_y[[(i << 0x01) % length, ((j >> 0x01) % length)]]);
+        for j in 1_000..2_000 {
+            let tmp = v_y[[idx[i % length], idx[j % length]]];
+            black_box(tmp);
         }
     }
 }
 
 // 3D vector access
-fn f3(size: u32) {
-    let length = 2_usize.pow(size);
-    let mut y: Vec<f64> = vec![0.0; length * length * length];
-    black_box(&mut y); // necessary?
+fn f3(length: usize, indices: &[usize]) {
+    let y: Vec<f64> = vec![0.0; length * length * length];
+    let idx = &indices[0..length];
 
-    // simulate 2 indexes ?
     for i in 0..100 {
-        for j in 0..100 {
-            for k in 0..100 {
-                black_box(
-                    y[((i << 0x01) % length) * length * length
-                        + ((j >> 0x01) % length) * length
-                        + ((k << 0x01) % length)],
-                );
+        for j in 100..200 {
+            for k in 200..300 {
+                // setting this as unchecked somehow produces the same perf as the 1D case
+                // this does not work in 2D; perf stays the same
+                // why?
+                let tmp = unsafe {
+                    y.get_unchecked(
+                        idx[i % length] * length * length
+                            + idx[j % length] * length
+                            + idx[k % length],
+                    )
+                };
+                black_box(tmp);
             }
         }
     }
 }
 
 // 3D view access
-fn f3_b(size: u32) {
-    let length = 2_usize.pow(size);
-    let mut v_y: ViewOwned<'_, 3, f64> = ViewOwned::new_from_data(
+fn f3_b(length: usize, indices: &[usize]) {
+    let v_y: ViewOwned<'_, 3, f64> = ViewOwned::new_from_data(
         vec![0.0; length * length * length],
         Layout::Right,
         [length, length, length],
     );
-    black_box(&mut v_y); // necessary?
+    let idx = &indices[0..length];
 
     for i in 0..100 {
-        for j in 0..100 {
-            for k in 0..100 {
-                black_box(
-                    v_y[[
-                        (i << 0x01) % length,
-                        (j >> 0x01) % length,
-                        (k << 0x01) % length,
-                    ]],
-                );
+        for j in 100..200 {
+            for k in 200..300 {
+                let tmp = v_y[[idx[i % length], idx[j % length], idx[k % length]]];
+                black_box(tmp);
             }
         }
     }
@@ -102,39 +100,48 @@ fn f3_b(size: u32) {
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     // Generate/Define the input
-    let data_size: u32 = 11; // 2048 length vector, 2048*2048 matrix
+    const DATA_SIZE: u32 = 11; // 2048 length vector, 2048*2048 matrix
+    let length = 2_usize.pow(DATA_SIZE);
+    let mut rng = SmallRng::from_entropy();
+    let indices: Vec<usize> = rand::seq::index::sample(&mut rng, length, length).into_vec();
 
     let mut group1 = c.benchmark_group("1D access");
     group1.bench_with_input(
         BenchmarkId::new("Vector Access", ""),
-        &data_size,
-        |b, &n| b.iter(|| f1(n)),
+        &(length, indices.clone()),
+        |b, (n, i)| b.iter(|| f1(*n, i)),
     );
-    group1.bench_with_input(BenchmarkId::new("View Access", ""), &data_size, |b, &n| {
-        b.iter(|| f1_b(n))
-    });
+    group1.bench_with_input(
+        BenchmarkId::new("View Access", ""),
+        &(length, indices.clone()),
+        |b, (n, i)| b.iter(|| f1_b(*n, i)),
+    );
     group1.finish();
 
     let mut group2 = c.benchmark_group("2D access");
     group2.bench_with_input(
         BenchmarkId::new("Vector Access", ""),
-        &data_size,
-        |b, &n| b.iter(|| f2(n)),
+        &(length, indices.clone()),
+        |b, (n, i)| b.iter(|| f2(*n, i)),
     );
-    group2.bench_with_input(BenchmarkId::new("View Access", ""), &data_size, |b, &n| {
-        b.iter(|| f2_b(n))
-    });
+    group2.bench_with_input(
+        BenchmarkId::new("View Access", ""),
+        &(length, indices.clone()),
+        |b, (n, i)| b.iter(|| f2_b(*n, i)),
+    );
     group2.finish();
 
     let mut group3 = c.benchmark_group("3D access");
     group3.bench_with_input(
         BenchmarkId::new("Vector Access", ""),
-        &data_size,
-        |b, &n| b.iter(|| f3(n)),
+        &(length, indices.clone()),
+        |b, (n, i)| b.iter(|| f3(*n, i)),
     );
-    group3.bench_with_input(BenchmarkId::new("View Access", ""), &data_size, |b, &n| {
-        b.iter(|| f3_b(n))
-    });
+    group3.bench_with_input(
+        BenchmarkId::new("View Access", ""),
+        &(length, indices.clone()),
+        |b, (n, i)| b.iter(|| f3_b(*n, i)),
+    );
     group3.finish();
 }
 
