a5a79cb96a44abd47de66470c5559ceefe79387c
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-10-12 10:58:06 +0200

    rewrote gemv benches to only use base types
    criterion produces a nice violin plot with performances for
    on the layout

--- benches/gemv.rs
+++ benches/gemv.rs
@@ -1,62 +1,79 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use poc_kokkos_rs::view::{parameters::Layout, ViewBase, ViewOwned};
 
-// regular matrix vector product
+// regular matrix-vector product
 fn f1(size: u32) {
     let length = 2_usize.pow(size);
     let x = vec![1.0; length];
     #[allow(non_snake_case)]
-    let A = vec![vec![1.0; length]; length];
+    let A = vec![1.0; length * length];
+    // in this case, we can use Rust's iterator directly to easily operate
+    // line by line.
     let y: Vec<f64> = A
-        .iter()
-        .map(|row| row.iter().zip(x.iter()).map(|(f1, f2)| f1 * f2).sum())
+        .chunks(length)
+        .map(|row| row.iter().zip(x.iter()).map(|(r_i, x_i)| r_i * x_i).sum())
         .collect();
     black_box(y);
 }
 
-// regular matrix vector product, with a flattened matrix
+// regular matrix-vector product; using indexes
 fn f1_b(size: u32) {
     let length = 2_usize.pow(size);
     let x = vec![1.0; length];
     #[allow(non_snake_case)]
     let A = vec![1.0; length * length];
-    let y: Vec<f64> = A
-        .chunks_exact(length)
-        .map(|row| row.iter().zip(x.iter()).map(|(f1, f2)| f1 * f2).sum())
-        .collect();
+    // As a reference, an implementation using indexes
+    let mut y: Vec<f64> = vec![0.0; length];
+    // col and row indexes of the matrix
+    for row in 0..length {
+        for col in 0..length {
+            // using unchecked accesses to keep the comparison "fair"
+            // as iterators bypass those
+            unsafe {
+                *y.get_unchecked_mut(row) +=
+                    A.get_unchecked(row * length + col) * x.get_unchecked(col)
+            }
+        }
+    }
     black_box(y);
 }
 
-// views vector matrix product
+// regular vector-matrix product
 fn f2(size: u32) {
     let length = 2_usize.pow(size);
     let x = vec![1.0; length];
     #[allow(non_snake_case)]
     let A = vec![1.0; length * length];
-    // in the case of vector-matrix product, we would like to iterate on the
-    // column of the matrix; We can't do that efficiently with the regular
-    // "row first" layout (i.e. a LayoutRight)
-    let v_x: ViewOwned<'_, 1, f64> = ViewBase::new_from_data(x, Layout::Right, [length]);
-    #[allow(non_snake_case)]
-    let v_A: ViewOwned<'_, 2, f64> = ViewBase::new_from_data(A, Layout::Right, [length, length]);
-    // result
-    let y: ViewOwned<'_, 1, f64> = ViewBase::new(Layout::Right, [length]);
+    // in the case of a vector-matrix product, the "row-first" layout (i.e. 2D LayoutRight)
+    // does not allow us to make use of Rust's iterators -> back to indexes
+    let mut y: Vec<f64> = vec![0.0; length];
+    // col and row indexes of the matrix
+    for col in 0..length {
+        for row in 0..length {
+            // using unchecked accesses to keep the comparison "fair"
+            // as iterators bypass those
+            unsafe {
+                *y.get_unchecked_mut(col) +=
+                    x.get_unchecked(row) * A.get_unchecked(row * length + col)
+            }
+        }
+    }
+    black_box(y);
 }
 
-// views vector matrix product with adapted layout
+// vector-matrix product with an adapted layout
 fn f3(size: u32) {
     let length = 2_usize.pow(size);
     let x = vec![1.0; length];
     #[allow(non_snake_case)]
     let A = vec![1.0; length * length];
-    // in the case of vector-matrix product, we would like to iterate on the
-    // column of the matrix; We can't do that efficiently with the regular
-    // "row first" layout (i.e. a LayoutRight)
-    let v_x: ViewOwned<'_, 1, f64> = ViewBase::new_from_data(x, Layout::Right, [length]);
-    #[allow(non_snake_case)]
-    let v_A: ViewOwned<'_, 2, f64> = ViewBase::new_from_data(A, Layout::Left, [length, length]);
-    // result
-    let y: ViewOwned<'_, 1, f64> = ViewBase::new(Layout::Right, [length]);
+    // Thanks to the "row first" layout (i.e. 2D LayoutLeft), we can use
+    // the iterators again
+    // The code is essentially the same as the matrix-vector product
+    let y: Vec<f64> = A
+        .chunks(length)
+        .map(|col| x.iter().zip(col.iter()).map(|(x_i, c_i)| x_i * c_i).sum())
+        .collect();
+    black_box(y);
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
@@ -65,29 +82,22 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("gemv");
     group.bench_with_input(
-        BenchmarkId::new("Regular Matrix-Vector Product", "number of iterations"),
+        BenchmarkId::new("Matrix-Vector Product (iterators)", ""),
         &data_size,
         |b, &n| b.iter(|| f1(n)),
     );
     group.bench_with_input(
-        BenchmarkId::new(
-            "Regular Matrix-Vector Product (Flattened Matrix)",
-            "number of iterations",
-        ),
+        BenchmarkId::new("Matrix-Vector Product (indexes)", ""),
         &data_size,
         |b, &n| b.iter(|| f1_b(n)),
     );
-
     group.bench_with_input(
-        BenchmarkId::new("Views Vector-Matrix Product", "number of iterations"),
+        BenchmarkId::new("Vector-Matrix Product (indexes)", ""),
         &data_size,
         |b, &n| b.iter(|| f2(n)),
     );
     group.bench_with_input(
-        BenchmarkId::new(
-            "Views Vector-Matrix Product with Adapted Layout ",
-            "number of iterations",
-        ),
+        BenchmarkId::new("Vector-Matrix Product w/ adapted layout (iterators)", ""),
         &data_size,
         |b, &n| b.iter(|| f3(n)),
     );
