ee7d088f620b672fde6ec71e86b91e84c3a5cb84
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-10-19 11:51:24 +0200

    removed functors implem
    Functor is now just an alias for FnMut; need to implem parallel
    statement before looking at functor

--- src/functor.rs
+++ src/functor.rs
@@ -4,23 +4,4 @@
 //!
 
 /// Functor trait. User can implement its own functor by implementing this trait.
-pub trait Functor<Args> {
-    type Output;
-
-    // forbidden becuase of https://github.com/rust-lang/rust/issues/91611
-    // fn closure(&self) -> (impl Fn(Args) -> Self::Output);
-    fn closure<'a, 'b>(&'a self) -> Box<dyn Fn(Args) -> Self::Output + 'b>
-    where
-        'a: 'b;
-}
-
-impl<Args, Out> Functor<Args> for dyn Fn(Args) -> Out {
-    type Output = Out;
-
-    fn closure<'a, 'b>(&'a self) -> Box<dyn Fn(Args) -> Self::Output + 'b>
-    where
-        'a: 'b,
-    {
-        Box::new(self)
-    }
-}
+pub trait Functor<Args, Output>: Fn(Args) -> Output {}
