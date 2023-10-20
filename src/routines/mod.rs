a911d5b00ef1937060c7a3c496402d360369a379
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-10-20 10:20:12 +0200

    parallel_for signature

--- src/routines/mod.rs
+++ src/routines/mod.rs
@@ -8,4 +8,19 @@
 //! Parameters of aforementionned statements are defined in the [`parameters`] sub-module.
 //!
 
+use self::parameters::{ExecutionPolicy, RangePolicy};
+
 pub mod parameters;
+
+pub fn parallel_for<const DEPTH: usize, R, F, E, Args, Error>(
+    execp: ExecutionPolicy<R>,
+    func: F,
+) -> Result<(), E>
+// potentially a handle in the reuslt if we can make the kernel execution async
+where
+    R: RangePolicy,
+    F: FnMut(Args), // for statement should not return a result?
+    E: std::error::Error,
+{
+    todo!()
+}
