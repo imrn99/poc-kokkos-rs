471413d259f9be0998f8d59b4879076f43b61e47
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-11-23 09:20:20 +0100

    gemm skeleton

---
+++ benches/gemm.rs
@@ -0,0 +1,154 @@
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use poc_kokkos_rs::{
+    functor::KernelArgs,
+    routines::{
+        parallel_for,
+        parameters::{ExecutionPolicy, ExecutionSpace, RangePolicy, Schedule},
+    },
+    view::{parameters::Layout, ViewOwned},
+};
+use rand::{
+    distributions::{Distribution, Uniform},
+    rngs::SmallRng,
+    SeedableRng,
+};
+
+// Serial GEMM
+fn f1(
+    length: usize,
+    aa_init: Vec<f64>,
+    bb_init: Vec<f64>,
+    cc_init: Vec<f64>,
+    alpha: f64,
+    beta: f64,
+) {
+    let mut aa = ViewOwned::new_from_data(aa_init, Layout::Right, [length, length]);
+    let mut bb = ViewOwned::new_from_data(bb_init, Layout::Right, [length, length]);
+    let mut cc = ViewOwned::new_from_data(cc_init, Layout::Right, [length, length]);
+    black_box(&mut aa);
+    black_box(&mut bb);
+    black_box(&mut cc);
+
+    let execp = ExecutionPolicy {
+        space: ExecutionSpace::Serial,
+        range: RangePolicy::RangePolicy(0..length),
+        schedule: Schedule::Static,
+    };
+
+    // y = alpha * A * x + beta * y
+    let gemm_kernel = |arg: KernelArgs<1>| match arg {
+        KernelArgs::Index1D(i) => {
+            todo!()
+        }
+        KernelArgs::IndexND(_) => unimplemented!(),
+        KernelArgs::Handle => unimplemented!(),
+    };
+    parallel_for(execp, gemm_kernel).unwrap();
+    black_box(&cc);
+}
+
+// DeviceCPU GEMM
+fn f2(
+    length: usize,
+    aa_init: Vec<f64>,
+    bb_init: Vec<f64>,
+    cc_init: Vec<f64>,
+    alpha: f64,
+    beta: f64,
+) {
+    let mut aa = ViewOwned::new_from_data(aa_init, Layout::Right, [length, length]);
+    let mut bb = ViewOwned::new_from_data(bb_init, Layout::Right, [length, length]);
+    let mut cc = ViewOwned::new_from_data(cc_init, Layout::Right, [length, length]);
+    black_box(&mut aa);
+    black_box(&mut bb);
+    black_box(&mut cc);
+
+    let execp = ExecutionPolicy {
+        space: ExecutionSpace::DeviceCPU,
+        range: RangePolicy::RangePolicy(0..length),
+        schedule: Schedule::Static,
+    };
+
+    // y = alpha * A * x + beta * y
+    let gemm_kernel = |arg: KernelArgs<1>| match arg {
+        KernelArgs::Index1D(i) => {
+            todo!()
+        }
+        KernelArgs::IndexND(_) => unimplemented!(),
+        KernelArgs::Handle => unimplemented!(),
+    };
+    parallel_for(execp, gemm_kernel).unwrap();
+    black_box(&cc);
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    // Generate/Define the input
+    const DATA_SIZE: u32 = 12;
+    let length = 2_usize.pow(DATA_SIZE);
+    let seed: u64 = 9817498146784;
+    let mut rng = SmallRng::seed_from_u64(seed);
+    let range: Uniform<f64> = rand::distributions::Uniform::new(0.0, 100.0);
+    let aa_init: Vec<f64> = (0..length * length)
+        .map(|_| range.sample(&mut rng))
+        .collect();
+    let bb_init: Vec<f64> = (0..length * length)
+        .map(|_| range.sample(&mut rng))
+        .collect();
+    let cc_init: Vec<f64> = (0..length * length)
+        .map(|_| range.sample(&mut rng))
+        .collect();
+    let alpha: f64 = range.sample(&mut rng);
+    let beta: f64 = range.sample(&mut rng);
+
+    let mut group = c.benchmark_group("gemm");
+    group.bench_with_input(
+        BenchmarkId::new("exec-serial", ""),
+        &(
+            length,
+            aa_init.clone(),
+            bb_init.clone(),
+            cc_init.clone(),
+            alpha,
+            beta,
+        ),
+        |b, (length, aa_init, bb_init, cc_init, alpha, beta)| {
+            b.iter(|| {
+                f1(
+                    *length,
+                    aa_init.clone(),
+                    bb_init.clone(),
+                    cc_init.clone(),
+                    *alpha,
+                    *beta,
+                )
+            })
+        },
+    );
+    group.bench_with_input(
+        BenchmarkId::new("exec-devicecpu", ""),
+        &(
+            length,
+            aa_init.clone(),
+            bb_init.clone(),
+            cc_init.clone(),
+            alpha,
+            beta,
+        ),
+        |b, (length, aa_init, bb_init, cc_init, alpha, beta)| {
+            b.iter(|| {
+                f2(
+                    *length,
+                    aa_init.clone(),
+                    bb_init.clone(),
+                    cc_init.clone(),
+                    *alpha,
+                    *beta,
+                )
+            })
+        },
+    );
+    group.finish()
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
