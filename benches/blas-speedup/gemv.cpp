8d0c13d8470edf8f93e0a3ed5ead7e49fdfb096b
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-11-24 16:28:05 +0100

    added gemv

--- benches/blas-speedup/gemv.cpp
+++ benches/blas-speedup/gemv.cpp
@@ -14,8 +14,9 @@
 
 #include <Kokkos_Core.hpp>
 
-#define DATA_SIZE 10
+#define DATA_SIZE 15
 #define N_REPEAT 100
+
 
 int main( int argc, char* argv[] )
 { 
@@ -23,31 +24,34 @@ int main( int argc, char* argv[] )
     {
         // Readability
         typedef Kokkos::View<double**, Kokkos::LayoutRight>  MatRight;
-        typedef Kokkos::View<double**, Kokkos::LayoutLeft>  MatLeft;
+        typedef Kokkos::View<double*, Kokkos::LayoutLeft>   VecColumn; // technically layout doesn't matter here
 
         // declare data
-
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_real_distribution<> dis(0.0, 1.0);
         const uint64_t length = pow(2, DATA_SIZE);
 
-        // runtime dims
         MatRight A("matA", length, length);
-        MatLeft B("matB", length, length);
-        MatRight C("matC", length, length);
+        VecColumn x("vecx", length);
+        VecColumn y("vecy", length);
         double alpha;
         double beta; 
 
         // fill with rand doubles
         alpha = dis(gen);
         beta = dis(gen);
-        for (int ii = 0; ii < length; ii++) {
-        for (int jj = 0; jj < length; jj++) {
-            A(ii,jj) = dis(gen);
-            B(ii,jj) = dis(gen);
-            C(ii,jj) = dis(gen);
-        }}
+        // need to parallelize this if possible
+        Kokkos::parallel_for("inits", length, KOKKOS_LAMBDA(const uint64_t ii) {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<> dis(0.0, 1.0);
+            x(ii) = dis(gen);
+            y(ii) = dis(gen);
+            for (int jj = 0; jj < length; jj++) {
+                A(ii,jj) = dis(gen);
+            }
+        });
 
         // run the kernel N_REPEAT times
 
@@ -56,17 +60,11 @@ int main( int argc, char* argv[] )
             
             const auto start{std::chrono::steady_clock::now()};   // start timer
             Kokkos::parallel_for("GEMM kernel", length, KOKKOS_LAMBDA(const uint64_t i) {
-                for (uint64_t j = 0; j < length; j++) {
-                    // this computation is the most costly part of the kernel
-                    // trying to turn this into a proper reduction significantly
-                    // obfuscate the code.
-                    // I think this is pretty interesting since it can be done with 
-                    // decent performances using just one line in Rust.
-                    double AB_ij = 0.0; 
-                    for (uint64_t k = 0; k < length; k++) { AB_ij += A(i,k) * B(k,j); }
-                    // assign to C
-                    C(i, j) = alpha * AB_ij + beta * C(i, j);
-                }
+                // compute (A*x)(i)
+                double Ax_i = 0.0;
+                for (uint64_t j = 0; j < length; j++) { Ax_i += A(i, j) * x(j); }
+                // assign to y
+                y(i) = alpha * Ax_i + beta * y(i);
             });
             const auto end{std::chrono::steady_clock::now()};     // end timer
 
