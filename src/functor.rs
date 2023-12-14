b4a8f9f74650cc182c1fb5c5419b46f34ab5441e
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-12-14 14:19:04 +0100

    updated functor docs

--- src/functor.rs
+++ src/functor.rs
@@ -3,6 +3,13 @@
 //! This module contains all functor and kernel related code. Its content
 //! is highly dependant on the features enabled since the traits that a
 //! kernel must satisfy changes totally depending on the backend used.
+//!
+//! Kernel signatures are handled using `cargo` features. Using conditionnal
+//! compilation, the exact trait kernels must implement are adjusted according
+//! to the backend used to dispatch statements.
+//!
+//! In order to have actual closures match the required trait implementation,
+//! the same mechanism is used to define operations on [`Views`][crate::view].
 
 #[cfg(doc)]
 use crate::routines::parameters::RangePolicy;
@@ -63,16 +70,49 @@ pub enum KernelArgs<const N: usize> {
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "rayon")] {
-        /// `rayon`-specific kernel type.
+        /// `parallel_for` kernel type. Depends on enabled feature(s).
+        ///
+        /// This type alias is configured according to enabled feature in order to adjust
+        /// the signatures of kernels to match the requirements of the underlying dispatch routines.
+        ///
+        /// ### Possible Values
+        /// - `rayon` feature enabled: `Box<dyn Fn(KernelArgs<N>) + Send + Sync + 'a>`
+        /// - `threads` feature enabled: `Box<dyn Fn(KernelArgs<N>) + Send + 'a>`
+        /// - no feature enabled: fall back to [`SerialForKernelType`][SerialForKernelType]
+        ///
+        /// Current version: `rayon`
         pub type ForKernelType<'a, const N: usize> = Box<dyn Fn(KernelArgs<N>) + Send + Sync + 'a>;
     } else if #[cfg(feature = "threads")] {
-        /// Standard threads specific kernel type.
+        /// `parallel_for` kernel type. Depends on enabled feature(s).
+        ///
+        /// This type alias is configured according to enabled feature in order to adjust
+        /// the signatures of kernels to match the requirements of the underlying dispatch routines.
+        ///
+        /// ### Possible Values
+        /// - `rayon` feature enabled: `Box<dyn Fn(KernelArgs<N>) + Send + Sync + 'a>`
+        /// - `threads` feature enabled: `Box<dyn Fn(KernelArgs<N>) + Send + 'a>`
+        /// - no feature enabled: fall back to [`SerialForKernelType`][SerialForKernelType]
+        ///
+        /// Current version: `threads`
         pub type ForKernelType<'a, const N: usize> = Box<dyn Fn(KernelArgs<N>) + Send + 'a>;
     } else {
-        /// Fall back kernel type.
+        /// `parallel_for` kernel type. Depends on enabled feature(s).
+        ///
+        /// This type alias is configured according to enabled feature in order to adjust
+        /// the signatures of kernels to match the requirements of the underlying dispatch routines.
+        ///
+        /// ### Possible Values
+        /// - `rayon` feature enabled: `Box<dyn Fn(KernelArgs<N>) + Send + Sync + 'a>`
+        /// - `threads` feature enabled: `Box<dyn Fn(KernelArgs<N>) + Send + 'a>`
+        /// - no feature enabled: fall back to [`SerialForKernelType`][SerialForKernelType]
+        ///
+        /// Current version: no feature
         pub type ForKernelType<'a, const N: usize> = SerialForKernelType<'a, N>;
     }
 }
 
-/// Serial kernel type.
+/// Serial kernel type. Does not depend on enabled feature(s).
+///
+/// This is the minimal required trait implementation for closures passed to a
+/// `for_each` statement.
 pub type SerialForKernelType<'a, const N: usize> = Box<dyn FnMut(KernelArgs<N>) + 'a>;
