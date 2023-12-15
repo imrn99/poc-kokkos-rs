9cfffa75e1d7b08e4d2bf289db88a4f557a9fbcd
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-12-15 14:12:52 +0100

    view module doc

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -4,19 +4,54 @@
 //! defined and used by the Kokkos library. There are different types of views, all
 //! implemented using the same backend, [ViewBase].
 //!
+//! Eventually, the different types of Views should be removed and replaced by a single
+//! type. The distinction between original and mirrors doesn't seem necessary in a Rust
+//! implementation where the ownership system handles all memory transaction.
+//!
+//! In order to have thread-safe structures to use in parallel statement, the inner data
+//! type of views is adjusted implicitly when compiling using parallelization features.
+//! To match the adjusted data type, view access is done through `get` and `set` methods,
+//! allowing for feature-specific mutability in signatures while keeping a consistent user
+//! API.
+//!
 //! Parameters of aforementionned views are defined in the [`parameters`] sub-module.
 //!
+//! ### Example
+//!
+//! Initialize and fill a 2D matrix:
+//! ```rust
+//! use poc_kokkos_rs::view::{
+//!     parameters::Layout,
+//!     ViewOwned,
+//! };
+//!
+//! let mut viewA: ViewOwned<'_, 2, f64> = ViewOwned::new(
+//!         Layout::Right, // see parameters & Kokkos doc
+//!         [3, 5],        // 3 rows, 5 columns
+//!     );
+//!
+//! for row in 0..3 {
+//!     for col in 0..5 {
+//!         viewA.set([row, col], row as f64);
+//!     }
+//! }
+//!
+//! // viewA:
+//! // (0.0 0.0 0.0 0.0 0.0)
+//! // (1.0 1.0 1.0 1.0 1.0)
+//! // (2.0 2.0 2.0 2.0 2.0)
+//! ```
 
 pub mod parameters;
 
 #[cfg(any(feature = "rayon", feature = "threads", feature = "gpu"))]
-use atomic::Atomic;
+use atomic::{Atomic, Ordering};
 
-#[cfg(not(any(feature = "rayon", feature = "threads", feature = "gpu")))]
+#[cfg(any(doc, not(any(feature = "rayon", feature = "threads", feature = "gpu"))))]
 use std::ops::IndexMut;
 
 use self::parameters::{compute_stride, DataTraits, DataType, InnerDataType, Layout};
-use std::{fmt::Debug, ops::Index, sync::Arc};
+use std::{fmt::Debug, ops::Index};
 
 #[derive(Debug)]
 /// Enum used to classify view-related errors.
@@ -27,7 +62,7 @@ pub enum ViewError<'a> {
     DoubleMirroring(&'a str),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 /// Common structure used as the backend of all `View` types. The main differences between
 /// usable types is the type of the `data` field.
 pub struct ViewBase<'a, const N: usize, T>
@@ -35,8 +70,7 @@ where
     T: DataTraits,
 {
     /// Data container. Depending on the type, it can be a vector (`Owned`), a reference
-    /// (`ReadOnly`), a mutable reference (`ReadWrite`) or an `Arc<>` pointing on a vector
-    /// (`Shared`).
+    /// (`ReadOnly`) or a mutable reference (`ReadWrite`).
     pub data: DataType<'a, T>,
     /// Memory layout of the view. Refer to Kokkos documentation for more information.
     pub layout: Layout<N>,
@@ -57,8 +91,7 @@ impl<'a, const N: usize, T> ViewBase<'a, N, T>
 where
     T: DataTraits, // fair assumption imo
 {
-    /// Constructor used to create owned (and shared?) views. See dedicated methods for
-    /// others.
+    /// Constructor used to create owned views. See dedicated methods for others.
     pub fn new(layout: Layout<N>, dim: [usize; N]) -> Self {
         // compute stride & capacity
         let stride = compute_stride(&dim, &layout);
@@ -73,8 +106,7 @@ where
         }
     }
 
-    /// Constructor used to create owned (and shared?) views. See dedicated methods for
-    /// others.
+    /// Constructor used to create owned views. See dedicated methods for others.
     pub fn new_from_data(data: Vec<T>, layout: Layout<N>, dim: [usize; N]) -> Self {
         // compute stride if necessary
         let stride = compute_stride(&dim, &layout);
@@ -99,8 +131,7 @@ impl<'a, const N: usize, T> ViewBase<'a, N, T>
 where
     T: DataTraits, // fair assumption imo
 {
-    /// Constructor used to create owned (and shared?) views. See dedicated methods for
-    /// others.
+    /// Constructor used to create owned views. See dedicated methods for others.
     pub fn new(layout: Layout<N>, dim: [usize; N]) -> Self {
         // compute stride & capacity
         let stride = compute_stride(&dim, &layout);
@@ -115,8 +146,7 @@ where
         }
     }
 
-    /// Constructor used to create owned (and shared?) views. See dedicated methods for
-    /// others.
+    /// Constructor used to create owned views. See dedicated methods for others.
     pub fn new_from_data(data: Vec<T>, layout: Layout<N>, dim: [usize; N]) -> Self {
         // compute stride if necessary
         let stride = compute_stride(&dim, &layout);
@@ -135,42 +165,89 @@ where
     }
 }
 
+// ~~~~~~~~ Uniform writing interface across all features
 impl<'a, const N: usize, T> ViewBase<'a, N, T>
 where
     T: DataTraits,
 {
-    // ~~~~~~~~ Uniform writing interface across all features
-
     #[inline(always)]
     #[cfg(not(any(feature = "rayon", feature = "threads", feature = "gpu")))]
-    /// Serial writing interface. Uses mutable indexing implementation.
+    /// Writing interface.
+    ///
+    /// Two different implementations of this method are defined in order to satisfy
+    /// the (im)mutability requirements when using parallelization features & keep a
+    /// consistent user API:
+    ///
+    /// - any feature enabled: implictly use an atomic store operation on top of the
+    /// regular [Index] trait implementation to prevent a mutable borrow. The store
+    /// currently uses relaxed ordering, this may change.
+    /// - no feature enabled: uses a regular [IndexMut] trait implementation.
+    ///
+    /// Note that [Index] is always implemented while [IndexMut] only is when no
+    /// features are enabled.
+    ///
+    /// Current version: no feature
     pub fn set(&mut self, index: [usize; N], val: T) {
         self[index] = val;
     }
 
     #[inline(always)]
     #[cfg(any(feature = "rayon", feature = "threads", feature = "gpu"))]
-    /// Thread-safe writing interface. Uses non-mutable indexing and
-    /// immutability of atomic type methods.
+    /// Writing interface.
     ///
-    /// Uses [atomic::Ordering::Relaxed], may be subject to change.
+    /// Two different implementations of this method are defined in order to satisfy
+    /// the (im)mutability requirements when using parallelization features & keep a
+    /// consistent user API:
+    ///
+    /// - any feature enabled: implictly use an atomic store operation on top of the
+    /// regular [Index] trait implementation to prevent a mutable borrow. The store
+    /// currently uses relaxed ordering, this may change.
+    /// - no feature enabled: uses a regular [IndexMut] trait implementation.
+    ///
+    /// Note that [Index] is always implemented while [IndexMut] only is when no
+    /// features are enabled.
+    ///
+    /// Current version: thread-safe
     pub fn set(&self, index: [usize; N], val: T) {
-        self[index].store(val, atomic::Ordering::Relaxed);
+        self[index].store(val, Ordering::Relaxed);
     }
 
     #[inline(always)]
     #[cfg(not(any(feature = "rayon", feature = "threads", feature = "gpu")))]
-    /// Serial writing interface. Uses mutable indexing implementation.
+    /// Reading interface.
+    ///
+    /// Two different implementations of this method are defined in order to keep a
+    /// consistent user API across features:
+    ///
+    /// - any feature enabled: implictly use an atomic load operation on top of the
+    /// regular [Index] trait implementation. The load currently uses relaxed ordering,
+    /// this may change.
+    /// - no feature enabled: uses the regular [Index] trait implementation.
+    ///
+    /// Note that [Index] is always implemented while [IndexMut] only is when no
+    /// features are enabled.
+    ///
+    /// Current version: no feature
     pub fn get(&self, index: [usize; N]) -> T {
         self[index]
     }
 
     #[inline(always)]
     #[cfg(any(feature = "rayon", feature = "threads", feature = "gpu"))]
-    /// Thread-safe writing interface. Uses non-mutable indexing and
-    /// immutability of atomic type methods.
+    /// Reading interface.
     ///
-    /// Uses [atomic::Ordering::Relaxed], may be subject to change.
+    /// Two different implementations of this method are defined in order to keep a
+    /// consistent user API across features:
+    ///
+    /// - any feature enabled: implictly use an atomic load operation on top of the
+    /// regular [Index] trait implementation. The load currently uses relaxed ordering,
+    /// this may change.
+    /// - no feature enabled: uses the regular [Index] trait implementation.
+    ///
+    /// Note that [Index] is always implemented while [IndexMut] only is when no
+    /// features are enabled.
+    ///
+    /// Current version: thread-safe
     pub fn get(&self, index: [usize; N]) -> T {
         self[index].load(atomic::Ordering::Relaxed)
     }
@@ -234,8 +311,14 @@ where
 
     // ~~~~~~~~ Convenience
 
-    #[cfg(not(any(feature = "rayon", feature = "threads", feature = "gpu")))]
-    pub fn raw_val<'b>(self) -> Result<Vec<InnerDataType<T>>, ViewError<'b>> {
+    #[cfg(all(
+        test,
+        not(any(feature = "rayon", feature = "threads", feature = "gpu"))
+    ))]
+    /// Consumes the view to return a `Vec` containing its raw data content.
+    ///
+    /// This method is meant to be used in tests
+    pub fn raw_val<'b>(self) -> Result<Vec<T>, ViewError<'b>> {
         if let DataType::Owned(v) = self.data {
             Ok(v)
         } else {
@@ -245,7 +328,10 @@ where
         }
     }
 
-    #[cfg(any(feature = "rayon", feature = "threads", feature = "gpu"))]
+    #[cfg(all(test, any(feature = "rayon", feature = "threads", feature = "gpu")))]
+    /// Consumes the view to return a `Vec` containing its raw data content.
+    ///
+    /// This method is meant to be used in tests
     pub fn raw_val<'b>(self) -> Result<Vec<T>, ViewError<'b>> {
         if let DataType::Owned(v) = self.data {
             Ok(v.iter()
@@ -259,6 +345,7 @@ where
     }
 
     #[inline(always)]
+    /// Mapping function between N-indices and the flat offset.
     pub fn flat_idx(&self, index: [usize; N]) -> usize {
         index
             .iter()
@@ -268,7 +355,7 @@ where
     }
 }
 
-/// Read-only access is always implemented.
+/// **Read-only access is always implemented.**
 impl<'a, const N: usize, T> Index<[usize; N]> for ViewBase<'a, N, T>
 where
     T: DataTraits,
@@ -295,8 +382,7 @@ where
 }
 
 #[cfg(not(any(feature = "rayon", feature = "threads", feature = "gpu")))]
-/// Read-write access is implemented using [IndexMut] trait when no parallel
-/// features are enabled.
+/// **Read-write access is only implemented when no parallel features are enabled.**
 impl<'a, const N: usize, T> IndexMut<[usize; N]> for ViewBase<'a, N, T>
 where
     T: DataTraits,
@@ -317,18 +403,6 @@ where
     }
 }
 
-impl<'a, const N: usize, T: PartialEq + Debug> PartialEq for ViewBase<'a, N, T>
-where
-    T: DataTraits,
-{
-    fn eq(&self, other: &Self) -> bool {
-        self.data == other.data
-            && self.layout == other.layout
-            && self.dim == other.dim
-            && self.stride == other.stride
-    }
-}
-
 /// View type owning the data it yields access to, i.e. "original" view.
 pub type ViewOwned<'a, const N: usize, T> = ViewBase<'a, N, T>;
 
@@ -339,7 +413,3 @@ pub type ViewRO<'a, const N: usize, T> = ViewBase<'a, N, T>;
 /// View type owning a mutable borrow to the data it yields access to, i.e. a
 /// read-write mirror.
 pub type ViewRW<'a, const N: usize, T> = ViewBase<'a, N, T>;
-
-/// View type owning a shared reference to the data it yields access to, i.e. a
-/// thread-safe read-only mirror. Is this useful ? Shouldn't this be `Arc<Mutex<T>>` ?
-pub type ViewShared<'a, const N: usize, T> = ViewBase<'a, N, Arc<T>>;
